0d2540652146df1a73bb2de6e8bcb3cb3025d462
Daniel Segovia <dsegovia@leftfieldlabs.com>
2018-04-08 18:48:05 -0700

    try using graphql

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const router = express.Router();
-const savingsRouter = require('./api/routes/savings');
+const apiEndpoint = require('./api/routes/savings');
 
 const mongoDB = 'mongodb://127.0.0.1/saver';
 mongoose.connect(mongoDB);
@@ -17,7 +17,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use(bodyParser.json());
 
-app.use('/api', savingsRouter);
+app.use('/api', apiEndpoint);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
